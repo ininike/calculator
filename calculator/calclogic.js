a3ac4c0fdaa8aec5d3b2b93caf01c7983f19757b
ininike <inioluwadenaike@gmail.com>
2024-07-25 09:32:37 +0100

    Add files via upload

---
+++ calculator/calclogic.js
@@ -0,0 +1,74 @@
+var init= false
+const screens = document.getElementById('screen')
+const input = document.querySelectorAll(".input");
+const evaluate= document.getElementById("evaluate")
+const AC=document.getElementById("AC")
+const del= document.getElementById("delete")
+function evals()
+{
+    let ans
+     try {
+         ans= eval(screens.innerText);
+         screens.innerText= ans;
+    } catch (error){
+        screens.innerText="error";
+        setTimeout(() => {
+            screens.innerText=""
+        },1000);
+    }
+        init= true;
+} 
+evaluate.addEventListener("click",evals) 
+
+
+input.forEach(btn => {
+
+  function operate(){
+    let val = btn.textContent
+
+    if(val == "X"){
+        screens.innerText += "*"
+    }else
+       screens.innerText += val
+    init= false;
+  }
+  btn.addEventListener("click", operate)
+    
+})
+
+
+function clear(){
+    screens.innerText= "";
+}
+AC.addEventListener("click",clear)
+
+function negative(){
+        screens.innerText=eval(screens.innerText+'*(-1)');
+        init=false;
+}
+neg.addEventListener("click",negative)
+
+function delet(){
+    let len= screens.innerText.length
+    let sub= screens.innerText.substring(0, (len-1));
+    screens.innerText= sub
+}
+del.addEventListener("click",delet);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+        
+  
